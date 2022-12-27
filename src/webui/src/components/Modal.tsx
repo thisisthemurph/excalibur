e67f89d3d10747d08bfe7b2d71b19e426052f931
Michael Murphy <michael.w.murphy@outlook.com>
2022-12-27 17:24:40 +0000

    Allows the modal to be maximised

--- src/webui/src/components/Modal.tsx
+++ src/webui/src/components/Modal.tsx
@@ -14,15 +14,27 @@ type Ref = HTMLDialogElement;
 // eslint-disable-next-line react/display-name
 const Modal = React.forwardRef<Ref, Props>(({ title, fullScreen, onClose, children }, ref) => {
 	const [isFullScreen, setIsFullScreen] = useState(fullScreen ?? false);
+	const [fullScreenOverrideStyles, setFullScreenOverrideStyles] = useState<string>("");
 
 	const handleClose = () => {
 		onClose();
 	};
 
+	useEffect(() => {
+		if (isFullScreen) {
+			document.body.classList.add("modal--open");
+			setFullScreenOverrideStyles("h-full");
+			return;
+		}
+
+		document.body.classList.remove("modal--open");
+		setFullScreenOverrideStyles("");
+	}, [isFullScreen, fullScreen]);
+
 	return (
 		<dialog
 			ref={ref}
-			className="w-max-10/12 overflow-y-auto overflow-x-hidden rounded-lg border border-gray-300 bg-white px-0 py-0 shadow-lg"
+			className={`w-max-10/12 overflow-y-auto overflow-x-hidden rounded-lg border border-gray-300 bg-white px-0 py-0 shadow-lg ${fullScreenOverrideStyles}`}
 		>
 			<div className="flex flex-col items-center justify-center "></div>
 			<header className="flex w-full justify-between">
@@ -30,7 +42,7 @@ const Modal = React.forwardRef<Ref, Props>(({ title, fullScreen, onClose, childr
 				<section className="flex w-auto justify-end">
 					<button
 						onClick={() => setIsFullScreen(!isFullScreen)}
-						className="flex aspect-square h-12 w-12 items-center justify-center bg-gray-400 px-1 py-1 text-white opacity-50 hover:opacity-100"
+						className="flex aspect-square h-8 w-8 items-center justify-center bg-gray-400 px-1 py-1 text-white opacity-50 hover:opacity-100"
 					>
 						{isFullScreen ? (
 							<ArrowDownIcon className="h-8 w-8" />
@@ -40,7 +52,7 @@ const Modal = React.forwardRef<Ref, Props>(({ title, fullScreen, onClose, childr
 					</button>
 					<button
 						onClick={handleClose}
-						className="flex aspect-square h-12 w-12 items-center justify-center rounded-tr-sm bg-red-500 px-1 py-1 text-white opacity-50 hover:opacity-100"
+						className="flex aspect-square h-8 w-8 items-center justify-center rounded-tr-sm bg-red-500 px-1 py-1 text-white opacity-50 hover:opacity-100"
 					>
 						<XMarkIcon className="h-8 w-8" />
 					</button>
