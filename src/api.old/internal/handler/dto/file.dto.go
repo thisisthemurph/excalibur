b86ce97adb528e16e2ba17560abacefdeabad706
Mike Murphy <michael.w.murphy@outlook.com>
2023-01-07 21:39:33 +0000

    Removes old Golang API code

--- src/api.old/internal/handler/dto/file.dto.go
+++
@@ -1,9 +0,0 @@
-package dto
-
-import "excalibur/internal/models"
-
-// FileUploadStatusDTO specifies the status of an uploaded file
-type FileUploadStatusDTO struct {
-	FileID string                  `json:"fileId"`
-	Status models.FileUploadStatus `json:"status"`
-}
