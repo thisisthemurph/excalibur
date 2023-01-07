86b66f02d6ab438841fc339e43f6cbba09a96995
Mike Murphy <michael.w.murphy@outlook.com>
2023-01-07 11:49:49 +0000

    Adds DTO for returning file upload status

---
+++ src/Excalibur.Api/DTOs/FileUploadStatusDto.cs
@@ -0,0 +1,12 @@
+﻿using Excalibur.Api.Models;
+
+namespace Excalibur.Api.DTOs;
+
+public class FileUploadStatusDto
+{
+    public string DataTemplateId { get; set; }
+
+    public string FileId { get; set; }
+
+    public string Status { get; set; } = FileUploadStatus.Unknown;
+}
